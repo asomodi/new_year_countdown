43b0e5b038419e414183cc1dbbea6668a47a2310
Akos Somodi <akossomodi@yahoo.com>
2020-04-01 19:04:34 +0200

    create dinamic year and spinner

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Set background year
+year.innerText = currentYear + 1;
+
 // Update countdown time
 function updateCountdown() {
   const currentTime = new Date();
@@ -27,4 +30,11 @@ function updateCountdown() {
   seconds.innerHTML = s < 10 ? '0' + s : s;
 }
 
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
+
+// Run every second
 setInterval(updateCountdown, 1000);
